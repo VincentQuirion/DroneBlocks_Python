1f2363fbffb19e6a08ccbb337cc334721d5546b6
Vincent Quirion <vincent.quirion@icloud.com>
2021-01-07 13:29:47 -0500

    The program can now be used by itself (there are still dependencies though).

--- DroneBlocks_Python/face_recognition.py
+++ DroneBlocks_Python/face_recognition.py
@@ -5,23 +5,23 @@ import time
 
 
 def init_face_recognition():
-    history = open("/Users/Vincent/Desktop/Programming/Drone/Drone_Blocks/face_history.pickle", "wb")
+    history = open("./pi-face-recognition/face_history.pickle", "wb")
     pickle.dump([], history)
     history.close()
-    current = open("/Users/Vincent/Desktop/Programming/Drone/Drone_Blocks/current_faces.pickle", "wb")
+    current = open("./pi-face-recognition/current_faces.pickle", "wb")
     pickle.dump([], current)
     current.close()
 
     def face_recognition():
         os.system(
-            'python /Users/Vincent/Desktop/pi-face-recognition/pi_face_recognition.py --cascade /Users/Vincent/Desktop/pi-face-recognition/haarcascade_frontalface_default.xml --encodings /Users/Vincent/Desktop/pi-face-recognition/encodings.pickle')
+            'python ./pi-face-recognition/pi_face_recognition.py --cascade ./pi-face-recognition/haarcascade_frontalface_default.xml --encodings ./pi-face-recognition/encodings.pickle')
     Thread(target=face_recognition).start()
 
 
 def get_current_faces():
 
     try:
-        file = open("/Users/Vincent/Desktop/Programming/Drone/Drone_Blocks/current_faces.pickle", "rb")
+        file = open("./pi-face-recognition/current_faces.pickle", "rb")
         faces = pickle.load(file)
         file.close()
         time.sleep(1)
@@ -31,7 +31,7 @@ def get_current_faces():
 
 
 def get_face_history():
-    file = open("/Users/Vincent/Desktop/Programming/Drone/Drone_Blocks/face_history.pickle", "rb")
+    file = open("./pi-face-recognition/face_history.pickle", "rb")
     faces = pickle.load(file)
     file.close()
     time.sleep(3)
